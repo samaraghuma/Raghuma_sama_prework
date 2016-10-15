03ac2a53bf7f128d0c6edd5a2e619d2e50956aaf
raghuma sama <raghuma sama>
2016-10-14 20:55:59 -0400

    First Commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Raghuma_sama_prework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int urNum;
+
+            int urdigits;
+            Console.Write(" Enter a number to reverse: ");
+            urNum = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Reversed Number: ");
+
+            do
+            {
+
+                urdigits = urNum % 10;
+
+                Console.Write(urdigits);
+
+                urNum = urNum / 10;
+
+            } while (urNum > 0);
+
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
